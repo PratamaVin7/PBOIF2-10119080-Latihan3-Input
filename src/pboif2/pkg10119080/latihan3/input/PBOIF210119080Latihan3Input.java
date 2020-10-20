efe1d6c5065d03f8a8c19748689c5a56793e0e08
unknown <kevinpratamaoke@gmail.com>
2020-10-20 13:16:52 +0700

    First commit

---
+++ src/pboif2/pkg10119080/latihan3/input/PBOIF210119080Latihan3Input.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119080.latihan3.input;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ * NAMA     : Kevin Pratama Sinaga
+ * KELAS    : PBOIF2
+ * NIM      : 10119080
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+
+public class PBOIF210119080Latihan3Input {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.print("Masukkan nama anda: ");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah " +nama);
+    }
+    
+}
